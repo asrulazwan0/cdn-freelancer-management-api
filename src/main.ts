68d182b8616bc10e948df0464e94ac6f44026bfa
Asrul Azwan <asrulazwan90@gmail.com>
2023-09-19 00:56:22 +0800

    feat: Enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
