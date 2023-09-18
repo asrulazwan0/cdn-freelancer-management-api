f957a2248396e2464a27ff7dc47396f10cfe72c6
Asrul Azwan <asrulazwan90@gmail.com>
2023-09-18 22:10:13 +0800

    feat: Add uniqueness constraint for email and username in user creation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -16,15 +16,18 @@ export class UsersService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    try {
-      const createdUser = new this.userModel(createUserDto);
-      return createdUser.save();
-    } catch (error) {
-      if (error.code === 11000) {
-        throw new ConflictException('Username already exists');
-      }
-      throw error;
+    const { username, email } = createUserDto;
+
+    const existingUser = await this.userModel.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Username or email is already in use');
     }
+
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
   }
 
   async getAllUsers(): Promise<User[]> {
@@ -40,14 +43,36 @@ export class UsersService {
   }
 
   async updateUser(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const { username, email } = updateUserDto;
+
+    const existingUserEmail = await this.userModel.findOne({
+      email,
+      _id: { $ne: id },
+    });
+
+    if (existingUserEmail) {
+      throw new ConflictException('Email is already in use');
+    }
+
+    const existingUserName = await this.userModel.findOne({
+      username,
+      _id: { $ne: id },
+    });
+
+    if (existingUserName) {
+      throw new ConflictException('Username is already in use');
+    }
+
     const updatedUser = await this.userModel.findByIdAndUpdate(
       id,
       updateUserDto,
       { new: true },
     );
+
     if (!updatedUser) {
       throw new NotFoundException('User not found');
     }
+
     return updatedUser;
   }
 
